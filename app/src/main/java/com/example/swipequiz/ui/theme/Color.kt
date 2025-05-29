09a2f49b436b0c9c74ec92a56890939c19d48e6e
Studggle <vg1992005@gmail.com>
2025-05-29 21:42:58 +0530

    Update Color.kt

--- app/src/main/java/com/example/swipequiz/ui/theme/Color.kt
+++ app/src/main/java/com/example/swipequiz/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val orange = Color(0xFFFF8F00)
+val black = Color(0xFF000000)
+val white = Color(0xFFFFFFFF)
+val grey = Color(0xF4F4F4)
